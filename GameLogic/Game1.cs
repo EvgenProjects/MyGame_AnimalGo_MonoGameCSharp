4544bf18e086a1d7682bf121fcde53c9c024436a
Evgen <evgchigir@gmail.com>
2023-08-14 20:44:22 +0300

    Added iOS project, added link to Content.mgcb

--- GameLogic/Game1.cs
+++ GameLogic/Game1.cs
@@ -21,7 +21,7 @@ namespace GameLogic
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
+            Content.RootDirectory = "ContentLink";
 
            // graphics.IsFullScreen = true;
             graphics.PreferredBackBufferWidth = 800;
---
+++ iOS/Program.cs
@@ -0,0 +1,34 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+using GameLogic;
+
+namespace iOS
+{
+    [Register("AppDelegate")]
+    internal class Program : UIApplicationDelegate
+    {
+        private static Game1 game;
+
+        internal static void RunGame()
+        {
+            game = new Game1();
+            game.IsMouseVisible = true; // show mouse
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, typeof(Program));
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
